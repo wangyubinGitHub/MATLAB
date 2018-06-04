b7cabd3d07eaab9ce74a5a02a87500e6b20a9eff
wang.yubin <wangyubin2010n@126.com>
2018-06-04 10:42:00 +0800

    sound2pitch ok

--- sound2pitch/main.m
+++ sound2pitch/main.m
@@ -2,12 +2,13 @@ function main()
 %MAIN 此处显示有关此函数的摘要
 %   此处显示详细说明
 % filename='../test.wav';
-% filename='../sourcefile/source1.wav';
-filename = '../sourcefile/tmpf.wav';
+  filename='../sourcefile/source1.wav';
+%  filename = '../sourcefile/tmpf.wav';
 minpitch=75;
 timeStep=0;
 maxpitch = 500;
 
+maxtime = 10.0;
 % 1
  [y, fs] = audioread(filename);
  if(fs ~= 44100)
@@ -15,7 +16,16 @@ maxpitch = 500;
  end
 %1 end
 
-sound_to_pitch(y,timeStep,minpitch,maxpitch);
+count  = ceil( length(y) / fs /maxtime);
 
+for i=1:count
+    startSample = 1+(i-1)*maxtime*fs;
+    endSample = (i)*maxtime*fs;
+    if(endSample > length(y))
+       endSample = length(y); 
+    end
+    y1 = y(startSample:endSample);
+    sound_to_pitch(y1,timeStep,minpitch,maxpitch);
+end
 end
 
